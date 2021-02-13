cf77490d3beb56f1e529b452d0f4bdaa3d3f0db0
rusiya <41211499+rusiya@users.noreply.github.com>
2021-02-13 06:58:29 -0800

    Button click and Forms Dynamic display of name on submit.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,16 @@ import React, { useState } from "react";
 function App() {
   const [headingText, setHeadingText] = useState("Hello");
   const [isMouseOver, setMouseOver] = useState(false);
+  const [name, setName] = useState("");
+  const [nameDisplay, setDisplayName] = useState("");
+
   //  const [isMouseOut, setMouseOut] = useState(false);
 
-  function handleClick() {
-    setHeadingText("Hello Again!");
+  function handleClick(event) {
+    //    setHeadingText("Hello Again!");
+    setDisplayName(name);
+
+    event.preventDefault(); //Prevents default behaviour which is refresh the page in case of forms.
   }
   function handleMouseOver() {
     setMouseOver(true);
@@ -15,10 +21,43 @@ function App() {
     //    setMouseOut(true);
     setMouseOver(false);
   }
+
+  function handleChange(event) {
+    //console.log("Changed");
+    //console.log(event.target.value);
+    setName(event.target.value);
+  }
   return (
     <div className="container">
-      <h1>{headingText}</h1>
-      <input type="text" placeholder="What's your name?" />
+      <h1>
+        {headingText} {nameDisplay} !
+      </h1>
+      <form onSubmit={handleClick}>
+        <input
+          type="text"
+          placeholder="What's your name?"
+          onChange={handleChange}
+          value={name}
+        />
+        <button
+          style={
+            { isMouseOver } && {
+              backgroundColor: isMouseOver ? "black" : "white"
+            }
+          }
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+      {/* <input
+        type="text"
+        placeholder="What's your name?"
+        onChange={handleChange}
+        value={name}
+      />
       <button
         style={
           { isMouseOver } && {
@@ -30,7 +69,7 @@ function App() {
         onMouseOut={handleMouseOut}
       >
         Submit
-      </button>
+      </button>       */}
     </div>
   );
 }
