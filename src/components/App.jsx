a7c253f6e4bb6cd93015e48835f4a8584c6fbc8f
rusiya <41211499+rusiya@users.noreply.github.com>
2021-02-13 06:33:14 -0800

    ReactEvents Conditional rendering inline styles event listener setting and using state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+  const [isMouseOver, setMouseOver] = useState(false);
+  //  const [isMouseOut, setMouseOut] = useState(false);
+
+  function handleClick() {
+    setHeadingText("Hello Again!");
+  }
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+  function handleMouseOut() {
+    //    setMouseOut(true);
+    setMouseOver(false);
+  }
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={
+          { isMouseOver } && {
+            backgroundColor: isMouseOver ? "black" : "white"
+          }
+        }
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        Submit
+      </button>
     </div>
   );
 }
